135f2a7b4604c267ca6b8205f2cc38b59adf0526
john <bblodfon@gmail.com>
2025-10-20 16:02:21 +0200

    small refinements

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,9 @@
 #' @import Matrix
 #' @import survival
 #' @import prodlim
+#' # Is prodlim used?
 # CHECK the ones below one-by-one!!!
 #' @importFrom graphics abline axis legend lines points text
 #' @importFrom stats as.dist cor hclust heatmap model.frame model.matrix model.response pchisq predict sd terms
+#' @importFrom grDevices gray
 "_PACKAGE"
